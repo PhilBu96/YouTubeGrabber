2fb1965eae85e608d0f0812af06c750fe41e65d0
Philipp Buthmann <nolimitsfan@googlemail.com>
2019-08-20 22:33:33 +0200

    No Input in URL field

--- YouTubeGrabber/MainForm.Designer.cs
+++ YouTubeGrabber/MainForm.Designer.cs
@@ -32,7 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.downloadButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox_inputUrl
@@ -73,22 +73,23 @@
             this.button1.Text = "Prüfen";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // downloadButton
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 482);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(760, 67);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Video herunterladen!";
-            this.button2.UseVisualStyleBackColor = true;
+            this.downloadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.downloadButton.Location = new System.Drawing.Point(12, 482);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(760, 67);
+            this.downloadButton.TabIndex = 4;
+            this.downloadButton.Text = "Video herunterladen!";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.downloadButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -109,7 +110,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button downloadButton;
     }
 }
 
--- YouTubeGrabber/MainForm.cs
+++ YouTubeGrabber/MainForm.cs
@@ -17,5 +17,29 @@ namespace YouTubeGrabber
             Console.WriteLine("Programm wird initialisiert...");
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Gibt true zurück, wenn das Video erfolgreich heruntergeladen wurde.
+        /// </summary>
+        /// <param name="url">Die YouTube-URL</param>
+        /// <returns></returns>
+        private bool DownloadVideo(String url)
+        {
+            bool success = false;
+
+            //Wenn nichts in der Text-Box eingegeben wurde
+            if (url == String.Empty || url == "\\s+")
+            {
+                //Gibt immer false zurück, wenn Bedingung erfüllt, da success am Start auf false gesetzt wird
+                return success;
+            }
+
+            return success;
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(DownloadVideo(textBox_inputUrl.Text));
+        }
     }
 }
